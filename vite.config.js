a86e6562f071ef0bc2c0719505fb7f8c21968c71
ShallaOE <riccardo.scialla@openeconomics.eu>
2026-05-29 18:02:09 +0200

    Fix Vercel base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Interreg-CENTRAL-EUROPE-Dashboard/',
+  base: process.env.VERCEL ? '/' : '/Interreg-CENTRAL-EUROPE-Dashboard/',
 })
